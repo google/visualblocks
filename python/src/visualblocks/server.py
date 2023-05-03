dad3d0b9e5e2a93c2bda738f54ea05cd5738990c
Jing Jin <8752427+jinjingforever@users.noreply.github.com>
2023-05-02 20:49:11 -0700

    Update server to support decorator

--- python/src/visualblocks/server.py
+++ python/src/visualblocks/server.py
@@ -8,6 +8,7 @@ from google.colab import output
 from google.colab.output import eval_js
 from IPython import display
 from IPython.utils import io
+from typing import Literal
 import json
 import logging
 import numpy as np
@@ -21,13 +22,63 @@ import traceback
 import urllib.parse
 import zipfile
 
-_VISUAL_BLOCKS_BUNDLE_VERSION = '1681885544'
+_VISUAL_BLOCKS_BUNDLE_VERSION = '1683083994'
 
 # Disable logging from werkzeug.
 #
 # Without this, flask will show a warning about using dev server (which is OK
 # in our usecase).
 logging.getLogger('werkzeug').disabled = True
+
+
+# Function registrations.
+GENERIC_FNS = {}
+TEXT_TO_TEXT_FNS = {}
+TEXT_TO_TENSORS_FNS = {}
+
+
+def register_vb_fn(type: Literal['generic', 'text_to_text', 'text_to_tensors']='generic'):
+  """A function decorator to register python function with Visual Blocks.
+
+  Args:
+    type:
+      the type of function to register for.
+
+      Currently, VB supports the following function types:
+
+      generic:
+        A function or iterable of functions, defined in the same Colab notebook,
+        that Visual Blocks can call to implement a generic model runner block.
+
+        A generic inference function must take a single argument, the input
+        tensors as an iterable of numpy.ndarrays; run inference; and return the
+        output tensors, also as an iterable of numpy.ndarrays.
+
+      text_to_text:
+        A function or iterable of functions, defined in the same Colab notebook,
+        that Visual Blocks can call to implement a text-to-text model runner
+        block.
+
+        A text_to_text function must take a string and return a string.
+
+      text_to_tensors:
+        A function or iterable of functions, defined in the same Colab notebook,
+        that Visual Blocks can call to implement a text-to-tensors model runner
+        block.
+
+        A text_to_tensors function must take a string and return the output
+        tensors, as an iterable of numpy.ndarrays.
+  """
+  def decorator_register_vb_fn(func):
+    func_name = func.__name__
+    if type == 'generic':
+      GENERIC_FNS[func_name] = func
+    elif type == 'text_to_text':
+      TEXT_TO_TEXT_FNS[func_name] = func
+    elif type == 'text_to_tensors':
+      TEXT_TO_TENSORS_FNS[func_name] = func
+    return func
+  return decorator_register_vb_fn
 
 
 def js(script):
@@ -63,18 +114,23 @@ def _make_json_response(obj):
 
 
 def _ensure_iterable(x):
-  """Turn x into an iterable if not already iterable, unless it is None."""
-  if x is None or hasattr(x, '__iter__'):
+  """Turn x into an iterable if not already iterable."""
+  if x is None:
+    return ()
+  elif hasattr(x, '__iter__'):
     return x
   else:
     return (x,)
 
 
-def _find_fn(name:str, functions):
-  """Find a function in a list of functions by its string name."""
-  names = [fn.__name__ for fn in functions]
-  i = names.index(name)
-  return functions[i]
+def _add_to_registry(fns, registry):
+  """Adds the functions to the given registry (dict)."""
+  for fn in fns:
+    registry[fn.__name__] = fn
+
+
+def _is_list_of_nd_array(obj):
+  return isinstance(obj, list) and all(isinstance(elem, np.ndarray) for elem in obj)
 
 
 def Server(generic=None, text_to_text=None, text_to_tensors=None, height=900, tmp_dir='/tmp', read_saved_pipeline=True):
@@ -82,7 +138,9 @@ def Server(generic=None, text_to_text=None, text_to_tensors=None, height=900, tm
 
   Other than serving the web app, it will also listen to requests sent from the
   web app at various API end points. Once a request is received, it will use the
-  data in the request body to call the corresponding functions passed in.
+  data in the request body to call the corresponding functions that users have
+  registered with VB, either through the '@register_vb_fn' decorator, or passed
+  in when creating the server.
 
   Args:
     generic:
@@ -118,9 +176,9 @@ def Server(generic=None, text_to_text=None, text_to_tensors=None, height=900, tm
       Whether to read the saved pipeline in the notebook or not.
   """
 
-  generic = _ensure_iterable(generic)
-  text_to_text = _ensure_iterable(text_to_text)
-  text_to_tensors = _ensure_iterable(text_to_tensors)
+  _add_to_registry(_ensure_iterable(generic), GENERIC_FNS)
+  _add_to_registry(_ensure_iterable(text_to_text), TEXT_TO_TEXT_FNS)
+  _add_to_registry(_ensure_iterable(text_to_tensors), TEXT_TO_TENSORS_FNS)
 
   app = Flask(__name__)
 
@@ -159,12 +217,15 @@ def Server(generic=None, text_to_text=None, text_to_tensors=None, height=900, tm
   @app.route('/api/list_inference_functions')
   def list_inference_functions():
     result = {}
-    if generic:
-      result['generic'] = [fn.__name__ for fn in generic]
-    if text_to_text:
-      result['text_to_text'] = [fn.__name__ for fn in text_to_text]
-    if text_to_tensors:
-      result['text_to_tensors'] = [fn.__name__ for fn in text_to_tensors]
+    if len(GENERIC_FNS):
+      result['generic'] = list(GENERIC_FNS.keys())
+      result['generic'].sort()
+    if len(TEXT_TO_TEXT_FNS):
+      result['text_to_text'] = list(TEXT_TO_TEXT_FNS.keys())
+      result['text_to_text'].sort()
+    if len(TEXT_TO_TENSORS_FNS):
+      result['text_to_tensors'] = list(TEXT_TO_TENSORS_FNS.keys())
+      result['text_to_tensors'].sort()
     return _make_json_response(result)
 
   # Note: using "/api/..." for POST requests is not allowed.
@@ -173,14 +234,14 @@ def Server(generic=None, text_to_text=None, text_to_tensors=None, height=900, tm
     """Handler for the generic api endpoint."""
     result = {}
     try:
-      try:
-        inference_fn = _find_fn(request.json['function'], generic)
-      except KeyError:
-        # TODO: remove this fallback try-block after .js implments function key
-        inference_fn = generic[0]
+      func_name = request.json['function']
+      inference_fn = GENERIC_FNS[func_name]
       input_tensors = [_json_to_ndarray(x) for x in request.json['tensors']]
       output_tensors = inference_fn(input_tensors)
-      result['tensors'] = [_ndarray_to_json(x) for x in output_tensors]
+      if not _is_list_of_nd_array(output_tensors):
+        result = {'error': 'The returned value from %s is not a list of ndarray' % func_name}
+      else:
+        result['tensors'] = [_ndarray_to_json(x) for x in output_tensors]
     except Exception as e:
       msg = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
       result = {'error': msg}
@@ -193,13 +254,14 @@ def Server(generic=None, text_to_text=None, text_to_tensors=None, height=900, tm
     """Handler for the text_to_text api endpoint."""
     result = {}
     try:
-      try:
-        inference_fn = _find_fn(request.json['function'], text_to_text)
-      except KeyError:
-        # TODO: remove this fallback try-block after .js implments function key
-        inference_fn = text_to_text[0]
+      func_name = request.json['function']
+      inference_fn = TEXT_TO_TEXT_FNS[func_name]
       text = request.json['text']
-      result['text'] = inference_fn(text)
+      ret = inference_fn(text)
+      if not isinstance(ret, str):
+        result = {'error': 'The returned value from %s is not a string' % func_name}
+      else:
+        result['text'] = ret
     except Exception as e:
       msg = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
       result = {'error': msg}
@@ -212,14 +274,14 @@ def Server(generic=None, text_to_text=None, text_to_tensors=None, height=900, tm
     """Handler for the text_to_tensors api endpoint."""
     result = {}
     try:
-      try:
-        inference_fn = _find_fn(request.json['function'], text_to_tensors)
-      except KeyError:
-        # TODO: remove this fallback try-block after .js implments function key
-        inference_fn = text_to_tensors[0]
+      func_name = request.json['function']
+      inference_fn = TEXT_TO_TENSORS_FNS[func_name]
       text = request.json['text']
       output_tensors = inference_fn(text)
-      result['tensors'] = [_ndarray_to_json(x) for x in output_tensors]
+      if not _is_list_of_nd_array(output_tensors):
+        result = {'error': 'The returned value from %s is not a list of ndarray' % func_name}
+      else:
+        result['tensors'] = [_ndarray_to_json(x) for x in output_tensors]
     except Exception as e:
       msg = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
       result = {'error': msg}
